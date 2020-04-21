145582ba7b7116323b200257050970a4d1034336
Andrey Baryshev <chebb@yandex.ru>
2020-04-21 11:40:52 +0400

    feat: date part

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year % 4 !== 0) return false;
+  if (year % 100 !== 0) return true;
+  if (year % 400 === 0) return true;
+  return false;
 }
 
 
@@ -73,8 +77,21 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let delta = endDate.getTime() - startDate.getTime();
+  let hr = Math.floor(delta / (1000 * 60 * 60));
+  delta -= hr * 1000 * 60 * 60;
+  let mn = Math.floor(delta / (1000 * 60));
+  delta -= mn * 1000 * 60;
+  let sec = Math.floor(delta / (1000));
+  delta -= sec * 1000;
+  if (hr < 10) hr = `0${hr}`;
+  if (mn < 10) mn = `0${mn}`;
+  if (sec < 10) sec = `0${sec}`;
+  if (delta < 10) {
+    delta = `00${delta}`;
+  } else if (delta < 100) delta = `0${delta}`;
+  return `${hr}:${mn}:${sec}.${delta}`;
 }
 
 
